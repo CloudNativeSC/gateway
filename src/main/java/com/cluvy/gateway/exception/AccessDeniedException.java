7a07a733590738aa282b83e200be81f52a86fe1a
melonhong <melonhong46@gmail.com>
2025-10-13 22:50:43 +0900

    feat: 예외 처리 로직
    - 기존에 권한이 없으면 HTTP Status만 보냈지만, 공통 응답 API가 추가됨에 따라 예외 처리 로직을 생성함.
    
    - JwtAuthenticationFilter: 권한이 없는 경우 JwtAuthenticationException 예외를 던짐.
    - GlobalErrorHandler: 모든 경로에 대한 에러를 처리하여 클라이언트에게 json 바디 응답을 포함하여 보냄.
    - JwtAuthenticationException: 401 에러, 권한이 없는 경우의 예외
    - AccessDeniedException: 403 에러, 페이지를 사용할 수 없는 경우의 예외
    - 예외들을 차후에 하나의 공통 클래스를 둘 예정임.

---
+++ src/main/java/com/cluvy/gateway/exception/AccessDeniedException.java
@@ -0,0 +1,16 @@
+package com.cluvy.gateway.exception;
+
+public class AccessDeniedException extends RuntimeException {
+
+    public AccessDeniedException() {
+        super();
+    }
+
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+
+    public AccessDeniedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
